19c102de52ba881fa29bb02e5524088d84581764
kshitij-nawandar9 <87687615+kshitij-nawandar9@users.noreply.github.com>
2021-08-20 00:35:51 +0530

    added routes

--- social-media/routes/api.php
+++ social-media/routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/users',[UserController::class,'getAllUsers']);
+Route::get('/users/{user}',[UserController::class,'getUserById']);
+Route::get('/users/{user}/posts',[PostController::class,'getAllPostsForUserId']);
+Route::get('/users/{user}/posts/{post}',[PostController::class,'getPostByIdForUserId']);
+Route::get('/posts/{post}/comments',[CommentController::class,'getAllCommentsForPostId']);
+Route::get('/posts/{post}/comment/{user}',[CommentController::class,'getCommentByIdForPostId']);
+
+Route::post('/user',[UserController::class, 'AddUser']);
+Route::post('/user/{user}/post',[PostController::class, 'AddPost']);
+Route::post('/post/{post}/comment',[CommentController::class, 'AddComment']);
+
+Route::delete('/user/{user}',[UserController::class, 'DeleteUser']);
+Route::delete('/post/{post}',[PostController::class, 'DeletePost']);
+Route::delete('/comment/{comment}',[CommentController::class, 'DeleteComment']);
